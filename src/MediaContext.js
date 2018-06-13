d4b8e5cfb5ebc6a38be6013b3bd401134ccac006
Emilio Srougo <Emilios1995@gmail.com>
2018-06-13 15:10:30 -0500

    Update MediaContext.js

--- src/MediaContext.js
+++ src/MediaContext.js
@@ -1,7 +1,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react'
-import { debounce } from 'lodash'
+import  debounce  from 'lodash.debounce'
 
 class MediaContext extends React.Component {
   constructor () {
